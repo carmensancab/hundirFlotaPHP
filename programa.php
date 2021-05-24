476499f221bdbc50baf3f8fc3c7d11c2efbc659d
carmensancab <carmen.sanchez.caballero@iesjulianmarias.es>
2021-05-24 16:19:51 +0200

    Cambiar ficheros

---
+++ programa.php
@@ -0,0 +1,55 @@
+<html>
+    <head>
+        <title>Piedra, Papel, Tijera, Lagarto, Spock</title>
+          <meta charset="utf-8"/>
+    </head>
+    <body background="huiga.jpg">
+        <?php
+            $respuesta=$_POST['pptls'];
+            if(isset($respuesta))
+            {
+               $rand= rand(0,4);
+                /* 0 piedra
+                   1 papel
+                   2 tijera
+                   3 lagarto
+                   4 spock*/
+                
+                // logica del juego
+                  if($rand==0 and $respuesta=="0" or $rand==1 and $respuesta=="1" or $rand==2 and $respuesta=="2" or $rand==3 and $respuesta=="3" or $rand==4 and $respuesta=="4")
+                  {
+                      echo "<img src=\"images/empate.jpg\" />";  
+                  }
+                elseif ($rand==0 and $respuesta==4 or $rand==0 and $respuesta==2 or $rand==1 and $respuesta==0 or $rand==1 and $respuesta==4 or $rand==2 and $respuesta==1 or $rand==2 and $respuesta==3 or $rand==3 and $respuesta==4 or $rand==3 and $respuesta==1 or $rand==4 and $respuesta==2 or $rand==4 and $respuesta==0)
+                {
+                    echo "<img src=\"images/win.jpg\" />";
+                    
+                }
+                else
+                {
+                    echo "<img src=\"images/perder.jpg\" />";
+                }
+                
+                
+                echo "<p>¿Quieres volver a jugar?";
+                //volver a jugar
+                echo "<form action=\"index.php\">";
+                echo "<input type=\"submit\" value=\"Si\"/></form>";
+                echo "<form action=\"pag3.html\">";
+                echo "<input type=\"submit\" value=\"No\"></form>"; 
+               
+               
+                
+            }
+            else
+            { 
+              
+                echo "<a href=\"index.html\">Debes Seleccionar una opción. </a>";
+            }
+        ?>
+    
+    
+    </body>
+
+
+</html>
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Para probar info.php
 for i in `seq 1 100`; do curl http://hundirflota-php-git-carmensancab-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/info.php; done
 
+
